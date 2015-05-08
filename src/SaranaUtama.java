c736835ad20794eeb9d5a387e83171c8fbe44c52
bayu FRNH <bulletfever12@gmail.com>
2015-05-08 06:40:36 -0700

    Update SaranaUtama.java

--- src/SaranaUtama.java
+++ src/SaranaUtama.java
@@ -1,15 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author samsung
- */
 import java.util.Scanner;
 public class SaranaUtama {
+    String kondisibaik;
+    String DipojokDanDekatDosen;
+    String BerfungsiDanDekatDosen;
     int Steker;
     String kondisiSteker;
     String posisiSteker;
@@ -25,9 +19,160 @@ public class SaranaUtama {
     int AC;
     String kondisiAC;
     String posisiAC;
+    String dibelakang_disamping;
     String SSID;
     int CCTV;
     String posisiCCTV;
+    private String atapruangan;
+    private String UMMhotspot;
+    private String depan_belakang;
+
+    public int getAC() {
+        return AC;
+    }
+
+    public void setAC(int AC) {
+        this.AC = AC;
+    }
+
+    public int getCCTV() {
+        return CCTV;
+    }
+
+    public void setCCTV(int CCTV) {
+        this.CCTV = CCTV;
+    }
+
+    public int getKipasAngin() {
+        return KipasAngin;
+    }
+
+    public void setKipasAngin(int KipasAngin) {
+        this.KipasAngin = KipasAngin;
+    }
+
+    public int getLampu() {
+        return Lampu;
+    }
+
+    public void setLampu(int Lampu) {
+        this.Lampu = Lampu;
+    }
+
+    public String getSSID() {
+        return SSID;
+    }
+
+    public void setSSID(String SSID) {
+        this.SSID = SSID;
+    }
+
+    public int getSteker() {
+        return Steker;
+    }
+
+    public void setSteker(int Steker) {
+        this.Steker = Steker;
+    }
+
+    public int getKabelLCD() {
+        return kabelLCD;
+    }
+
+    public void setKabelLCD(int kabelLCD) {
+        this.kabelLCD = kabelLCD;
+    }
+
+    public String getKondisiAC() {
+        return kondisiAC;
+    }
+
+    public void setKondisiAC(String kondisiAC) {
+        this.kondisiAC = kondisiAC;
+    }
+
+    public String getKondisiKabelLCD() {
+        return kondisiKabelLCD;
+    }
+
+    public void setKondisiKabelLCD(String kondisiKabelLCD) {
+        this.kondisiKabelLCD = kondisiKabelLCD;
+    }
+
+    public String getKondisiKipasAngin() {
+        return kondisiKipasAngin;
+    }
+
+    public void setKondisiKipasAngin(String kondisiKipasAngin) {
+        this.kondisiKipasAngin = kondisiKipasAngin;
+    }
+
+    public String getKondisiLampu() {
+        return kondisiLampu;
+    }
+
+    public void setKondisiLampu(String kondisiLampu) {
+        this.kondisiLampu = kondisiLampu;
+    }
+
+    public String getKondisiSteker() {
+        return kondisiSteker;
+    }
+
+    public void setKondisiSteker(String kondisiSteker) {
+        this.kondisiSteker = kondisiSteker;
+    }
+
+    public String getPosisiAC() {
+        return posisiAC;
+    }
+
+    public void setPosisiAC(String posisiAC) {
+        this.posisiAC = posisiAC;
+    }
+
+    public String getPosisiCCTV() {
+        return posisiCCTV;
+    }
+
+    public void setPosisiCCTV(String posisiCCTV) {
+        this.posisiCCTV = posisiCCTV;
+    }
+
+    public String getPosisiKabelLCD() {
+        return posisiKabelLCD;
+    }
+
+    public void setPosisiKabelLCD(String posisiKabelLCD) {
+        this.posisiKabelLCD = posisiKabelLCD;
+    }
+
+    public String getPosisiKipasAngin() {
+        return posisiKipasAngin;
+    }
+
+    public void setPosisiKipasAngin(String posisiKipasAngin) {
+        this.posisiKipasAngin = posisiKipasAngin;
+    }
+
+    public String getposisiLampu() {
+        return posisiLampu;
+    }
+
+    public void setposisiLampu(String posisiLampu) {
+        this.posisiLampu = posisiLampu;
+    }
+
+    public String getPosisiSteker() {
+        return posisiSteker;
+    }
+
+    public void setPosisiSteker(String posisiSteker) {
+        this.posisiSteker = posisiSteker;
+    }
+    public String getkondisiLampu(){
+        return kondisiLampu;
+    }
     
     void masuk(){
         Scanner in=new Scanner(System.in);
@@ -77,4 +222,131 @@ public class SaranaUtama {
         
         
     }
+     boolean Steker(){
+        if(getSteker()>= 4){
+            System.out.println("jumlah sesuai");
+        }
+        else{
+            System.out.println("jumlah tidak sesuai");
+        }
+        return true;
+      }
+      boolean PosisiSteker(){
+        if(getPosisiSteker() == DipojokDanDekatDosen ){
+            System.out.println("posisi sesuai.");
+        }
+        else{
+            System.out.println("posisi tidak sesuai");
+        }
+        return true;
+      }
+      boolean KabelLCD(){
+          if(getKabelLCD()>=1){
+             System.out.println("Jumlah Sesuai");
+          }
+          else{
+              System.out.println("Jumlah tidak sesuai");
+          }
+          return true;
+      }
+      boolean PosisiKabelLCD(){
+          if(getPosisiKabelLCD().equals(BerfungsiDanDekatDosen)){
+              System.out.println("Berfungsi");
+          }
+          else{
+              System.out.println("Tidak Berfungsi");
+          }
+          return true;
+      }
+      boolean Lampu(){
+          if(getLampu()>=18){
+              System.out.println("Jumlah Sesuai ");
+          }
+          else{
+              System.out.println("jumkah tidak sesuai");
+          }
+          return true;
+      }
+      boolean kondisiLampu(){
+          if(getKondisiLampu()== kondisibaik){
+              System.out.println("Sesuai");              
+          }
+          else{
+              System.out.println("Tidak sesuai");
+          }
+          return true;
+      }
+      
+      boolean posisiLampu(){
+          if(getposisiLampu() == atapruangan){
+              System.out.println("Sesuai");
+          }
+          else{
+              System.out.println("Tidak Sesuai");
+          }
+          return true;
+      }
+      boolean KipasAngin(){
+          if(getKipasAngin()>=2){
+              System.out.println("sesuai");
+          }
+          else{
+              System.out.println("Tidak sesuai");
+          }
+          return true;
+      }
+      boolean posisiKipasAngin(){
+          if(getPosisiKipasAngin()== atapruangan){
+              System.out.println("Sesuai");
+          }
+          else{
+              System.out.println("Tidak sesuai");
+          }
+          return true;
+      }
+      boolean AC(){
+          if(getAC()>=1){
+              System.out.println("Sesuai");
+          }
+          else{
+              System.out.println("Tidak sesuai");
+          }
+          return true;
+      }
+      boolean posisiAC(){
+          if(getPosisiAC().equals(dibelakang_disamping)){
+              System.out.println("Sesuai");
+          }
+          else{
+              System.out.println("Tidak sesuai");
+          }
+          return true;
+      }
+      boolean SSID(){
+          if(getSSID()==UMMhotspot){
+              System.out.println("Sesuai");
+          }
+          else{
+              System.out.println("Tidak sesuai");
+          }
+          return true;
+      }
+      boolean CCTV(){
+          if(getCCTV()==2){
+             System.out.println("Sesuai"); 
+          }
+          else{
+              System.out.println("Tidak Sesuai");
+          }
+          return true;
+      }
+      boolean PosisiCCTV(){
+          if(getPosisiCCTV()==depan_belakang){
+              System.out.println("Sesuai");
+          }
+          else{
+              System.out.println("Tidak sesuai");
+          }
+          return true;
+      }
 }
