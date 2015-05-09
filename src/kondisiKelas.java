d5e593a57aef9be1f67e15d3852489a544215f6a
bayu FRNH <bulletfever12@gmail.com>
2015-05-09 07:36:00 -0700

    Update kondisiKelas.java

--- src/kondisiKelas.java
+++ src/kondisiKelas.java
@@ -78,52 +78,113 @@ public class kondisiKelas {
      
     boolean analisiskebersihan1(){
         if(getLantai()== bersih){
-            System.out.println("kondisi lantai sesuai.");
+            System.out.println(outanalisiskebersihan1());
+            return true;
         }
         else{
-            System.out.println("kondisi lantai tidak sesuai");
+            System.out.println(out1analisiskebersihan1());
+            return true;
         }
-        return true;
+        
     }
+      public static boolean outanalisiskebersihan1(){
+          System.out.println("kondisi lantai sesuai.");
+          return true;  
+      }
+       public static boolean out1analisiskebersihan1(){
+          System.out.println("kondisi lantai tidak sesuai"); 
+        return false;
+       }
+       
+       
+       
     boolean analisiskebersihan2(){
         if(getDinding()== bersih){
-            System.out.println("kondisi dinding sesuai.");
+            System.out.println(outanalisiskebersihan2());
+             return true;
         }
         else{
-            System.out.println("kondisi dinding tidak sesuai");
+            System.out.println(out1analisiskebersihan2());
+             return false;  
         }
-        return true;
-    }
-    
+    }  
+      public static boolean outanalisiskebersihan2(){
+          System.out.println("kondisi dinding sesuai.");
+          return true;  
+      }
+       public static boolean out1analisiskebersihan2(){
+          System.out.println("kondisi dinding tidak sesuai"); 
+        return false;
+       }
+       
+       
+       
+       
     boolean analisiskebersiahan3(){
         if(getAtap()== bersih){
-            System.out.println("kondisi atap sesuai.");
+            System.out.println(outanalisiskebersihan3());
+            return true;
         }
         else{
-            System.out.println("kondisi atap tidak sesuai.");
+            System.out.println(out1analisiskebersihan3());
+            return false;
         }
-        return true;
     }
         
+      public static boolean outanalisiskebersihan3(){
+          System.out.println("kondisi atap sesuai.");
+          return true;  
+      }
+       public static boolean out1analisiskebersihan3(){
+          System.out.println("kondisi atap tidak sesuai"); 
+        return false;
+       }
+       
+       
+       
+       
     boolean analisiskebersihan4(){
         if(getPintu()== bersih){
-            System.out.println("kondisi pintu sesuai.");
+            System.out.println(outanalisiskebersihan4());
+            return true;
         }
         else{
-            System.out.println("kondisi pintu tidak sesuai.");
+            System.out.println(out1analisiskebersihan4());
+            return false;
         }
-        return true;
+   
     }
-    
+      public static boolean outanalisiskebersihan4(){
+          System.out.println("kondisi pintu sesuai.");
+          return true;  
+      }
+       public static boolean out1analisiskebersihan4(){
+          System.out.println("kondisi pintu tidak sesuai"); 
+        return false;
+       }
+       
+       
+       
+       
+     
     boolean analisiskebersiahan5(){    
         if(getJendela()== bersih){
-            System.out.println("kondisi jendela sesuai.");
+            System.out.println(outanalisiskebersihan5());
+             return true;
         }
         else{
-            System.out.println("kondisi jendela tidak sesuai.");
+            System.out.println(out1analisiskebersihan5());
+            return false;
         }
-        return true;
+       
     }
-    
+    public static boolean outanalisiskebersihan5(){
+          System.out.println("kondisi pintu sesuai.");
+          return true;  
+      }
+       public static boolean out1analisiskebersihan5(){
+          System.out.println("kondisi pintu tidak sesuai"); 
+        return false;
+       }
 }
 
